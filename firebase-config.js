d4eb2a87b291e91e3b5e8bdaf1165015d3624eeb
Saif Anwar <122825099+Saifanw@users.noreply.github.com>
2026-09-23 16:28:34 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDO75-iuVlRP9emqnR-d1T4g66akuQS328",
+  authDomain: "rajasthan-job.firebaseapp.com",
+  projectId: "rajasthan-job",
+  storageBucket: "rajasthan-job.firebasestorage.app",
+  messagingSenderId: "894888753938",
+  appId: "1:894888753938:web:92e66d7a7237be1fd9d1cb",
+  measurementId: "G-15S2XJQK80"
+};
